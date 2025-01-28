08a26598255bb1aaadaeaf9354c34c8a57e0f588
loprw <loprwdemileto@gmail.com>
2025-01-28 17:46:49 +0100

    Clase 28/01/2025
    Se modifica la clase UtilidadesExamen,java y Examen.java

--- src/davidjuan/morenoriera/examen/Examen.java
+++ src/davidjuan/morenoriera/examen/Examen.java
@@ -9,13 +9,13 @@ public class Examen {
 		System.out.println("Los numeros que componen el array son:");
 		UtilidadesExamen.mostrarArray(numeros);
 
-		System.out.println(UtilidadesExamen.calcularPromedio(numeros));
+		double promedio = UtilidadesExamen.calcularPromedio(numeros);
+		System.out.println(promedio);
 
 		int[] arrayExtremos = UtilidadesExamen.encontrarExtremos(numeros);
 		System.out.println("El número mayor del array es " + arrayExtremos[0] + ".\nEl número menor del array es "
 				+ arrayExtremos[1] + ".");
 
-		double promedio = UtilidadesExamen.calcularPromedio(numeros);
 		int[] numsMayorPromedio = UtilidadesExamen.filtrarMayoresQuePromedio(numeros, promedio);
 		System.out.println("Los numeros que componen el array de números mayores que el promedio son:");
 		UtilidadesExamen.mostrarArray(numsMayorPromedio);	
